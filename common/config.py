5deb27a6663bbc727c987243e15253f02150dbf2
Manu-Raj <maniishraj10425@yahoo.com>
2026-03-23 21:29:01 +0530

    updated config

--- common/config.py
+++ common/config.py
@@ -2,7 +2,7 @@ SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8000
 
 # 🔥 Replace with your hotspot IP
-SERVER_URL = "http:// 192.168.0.101:8000"
+SERVER_URL = "http://192.168.0.0:8000"
 
 DB_FILE = "telemetry.db"
 
